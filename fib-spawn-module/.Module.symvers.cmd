2684eff8a65fd2d13a3375fb242acba8095df0b4
Pranavjeet-Naidu <pranavjeetnaidu@gmail.com>
2025-04-16 12:56:20 +0530

    works :D

--- fib-spawn-module/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_Module.symvers :=  /usr/src/kernels/6.13.9-200.fc41.x86_64/scripts/mod/modpost -M        -o Module.symvers -n -T modules.order -i /usr/src/kernels/6.13.9-200.fc41.x86_64/Module.symvers -e 
--- fib-spawn-module/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo spawn.o; :; } > modules.order
